877d3455f6bf2f984e257987f05524420187fbdd
yunting wu <surprisewu@gmail.com>
2021-03-15 23:09:46 +0800

    feat: complete Week14

---
+++ Week14/dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/***/ (() => {
+
+eval("function createElement(type, attributes) {\n  var element = document.createElement(type);\n\n  for (var name in attributes) {\n    element.setAttribute(name, attributes[name]);\n  }\n\n  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {\n    children[_key - 2] = arguments[_key];\n  }\n\n  for (var _i = 0, _children = children; _i < _children.length; _i++) {\n    var child = _children[_i];\n\n    if (typeof child === 'string') {\n      child = document.createTextNode(child);\n    }\n\n    element.appendChild(child);\n  }\n\n  return element;\n}\n\nvar b = createElement(\"div\", {\n  id: \"a\"\n}, createElement(\"span\", null, \"1\"), createElement(\"span\", null, \"2\"), createElement(\"span\", null, \"3\"));\nvar container = createElement('div', {\n  id: 'a'\n}, 'hello world!');\ndocument.body.appendChild(container);\n\n//# sourceURL=webpack://week14/./main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./main.js"]();
+/******/ 	
+/******/ })()
+;
